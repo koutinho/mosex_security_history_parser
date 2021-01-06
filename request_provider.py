81669dd391373c086a65650e517b40e53d408537
a.vasiliev <a.vasiliev@uni-eng.ru>
2021-01-06 09:01:14 +0000

    mosex_request_provider_added

---
+++ request_provider.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_price_sheet(stock_name, candle_numbers, till):
+    url = f'{PRICE_SHEET_URL}/stock_name.json'
+    payload = {'s1.type': 'candles', 'interval': '1', 'candles': candle_numbers, 'till': till}
+    r = requests.get(url, params = payload)
+    
+    if r.status_code ==200:
+        return r.json()
+
+    return None
+
+PRICE_SHEET_URL = "https://iss.moex.com/cs/engines/stock/markets/shares/boardgroups/57/securities"
